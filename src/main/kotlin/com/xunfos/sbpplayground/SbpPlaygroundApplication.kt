12f68d9d4caefc72531c72baacef2f4e8760cd7d
Renato Costa <renato.costa@jimdo.com>
2020-06-24 21:15:23 +0200

    initial commit

---
+++ src/main/kotlin/com/xunfos/sbpplayground/SbpPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.xunfos.sbpplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbpPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<SbpPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/com/xunfos/sbpplayground/SbpPlaygroundApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.xunfos.sbpplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SbpPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
